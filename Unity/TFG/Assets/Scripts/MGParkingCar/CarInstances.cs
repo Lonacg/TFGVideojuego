48c42044e848208d8705bc0aaaaa2b0d261f554d
Lonacg <laurapcg36@gmail.com>
2024-08-21 14:54:12 +0200

    MGParking: Borrados los numeros de aparcamiento ocupados

--- Unity/TFG/Assets/Scripts/MGParkingCar/CarInstances.cs
+++ Unity/TFG/Assets/Scripts/MGParkingCar/CarInstances.cs
@@ -1,32 +1,39 @@
 using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class CarInstances : MonoBehaviour
 {
 
     public GameObject[] cars;
-
-    private GameObject parkingLot;
-    string pLot = "ParkingLot";
     public List<GameObject> carPositions;
+
+
+    public GameObject parkingLot;
+    //private string pLot = "ParkingLot";
+
+    public GameObject parkingNumber;
+    //private string pNum = "ParkingNumbers";
+    public List<GameObject> pNumbers;
 
 
 
     public void intanciateCars(){
 
-        // Asignamos las posiciones a la lista por codigo
-        parkingLot = GameObject.Find(pLot);
+        // Opcional: pasar la referencia por codigo en vez de en el inspector (menos coste no haciendolo)
+        //parkingLot = GameObject.Find(pLot);
+        //parkingNumber = GameObject.Find(pNum);
 
+        // Asignamos las posiciones a las listas por codigo
         for (int i = 0 ; i < parkingLot.transform.childCount; i++ ){
             carPositions.Add(parkingLot.transform.GetChild(i).gameObject); 
+            pNumbers.Add(parkingNumber.transform.GetChild(i).gameObject);
         }
 
         
         // Instanciamos los prefabs
         foreach (var car in cars){
-            // Lugar para instanciar
+            // Lugar random para instanciar
             int i = UnityEngine.Random.Range(0, carPositions.Count);
             GameObject placeToInstantiate = carPositions[i];
 
@@ -42,9 +49,13 @@ public class CarInstances : MonoBehaviour
             // Instancia
             Instantiate(car, placeToInstantiate.transform.position, Quaternion.Euler(0, rotChosen, 0));
 
-            // Eliminamos el GameObject de esa posicion y la quitamos de la lista
+            // Eliminamos el Lot de esa posicion y lo quitamos de la lista
             Destroy(carPositions[i]);
             carPositions.RemoveAt(i);
+
+            // Eliminamos ese numero de aparcamiento en ParkingNumbers
+            Destroy(pNumbers[i]);
+            pNumbers.RemoveAt(i);
         }
     }
 
