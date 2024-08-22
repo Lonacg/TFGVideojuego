de01f150abf77b9c2cc65bd0ba1680ce4f371a7f
Lonacg <laurapcg36@gmail.com>
2024-08-22 18:17:16 +0200

    MGParking: numeros de parking generados correctamente, pendiente gestionar la accion de victoria o derrota con el tag del parking

--- Unity/TFG/Assets/Scripts/MGParkingCar/CarInstances.cs
+++ Unity/TFG/Assets/Scripts/MGParkingCar/CarInstances.cs
@@ -5,7 +5,7 @@ public class CarInstances : MonoBehaviour
 {
 
     public GameObject[] cars;
-    public List<GameObject> carPositions;
+
 
 
     public GameObject parkingLot;
@@ -13,6 +13,7 @@ public class CarInstances : MonoBehaviour
 
     public GameObject parkingNumber;
     //private string pNum = "ParkingNumbers";
+    public List<GameObject> carPositions;
     public List<GameObject> pNumbers;
 
 
@@ -61,9 +62,50 @@ public class CarInstances : MonoBehaviour
     void Awake()
     {
         // Instanciamos los vehiculos de atrezo
-        intanciateCars();
-    }
 
+        // Opcional: pasar la referencia por codigo en vez de en el inspector (menos coste no haciendolo)
+        //parkingLot = GameObject.Find(pLot);
+        //parkingNumber = GameObject.Find(pNum);
+
+        // Asignamos las posiciones a las listas por codigo
+        for (int i = 0 ; i < parkingLot.transform.childCount; i++ ){
+            carPositions.Add(parkingLot.transform.GetChild(i).gameObject); 
+            pNumbers.Add(parkingNumber.transform.GetChild(i).gameObject);
+        }
+
+        
+        // Instanciamos los prefabs
+        foreach (var car in cars){
+            // Lugar random para instanciar
+            int i = Random.Range(0, carPositions.Count);
+            GameObject placeToInstantiate = carPositions[i];
+
+            // Rotacion del vehiculo prefab
+            int j = Random.Range(0,2);
+            int rotChosen;
+            if(j == 0){
+                rotChosen = 0;
+            }
+            else
+                rotChosen = 180;
+
+            // Instancia
+            Instantiate(car, placeToInstantiate.transform.position, Quaternion.Euler(0, rotChosen, 0));
+
+            // Eliminamos el Lot de esa posicion y lo quitamos de la lista
+            Destroy(carPositions[i]);
+            carPositions.RemoveAt(i);
+
+            // Eliminamos ese numero de aparcamiento en ParkingNumbers
+            Destroy(pNumbers[i]);
+            pNumbers.RemoveAt(i);
+
+        }
+
+
+
+
+    }
     void Update()
     {
         
--- Unity/TFG/Assets/Scripts/MGParkingCar/SetOperation.cs
+++ Unity/TFG/Assets/Scripts/MGParkingCar/SetOperation.cs
@@ -1,25 +1,47 @@
 using UnityEngine;
+using System.Linq;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
+using Unity.VisualScripting;
 
 public class SetOperation : MonoBehaviour
 {
 
     public GameObject parkingNumber;
+    //public GameObject[] parkingNumbers;
+    public CarInstances scriptCarInstances;
+
+    public int numberFreeParkings;
+    public List<GameObject> parkingNumbers;
 
     private int firstNumber;
     private int secondNumber;
     private int sol;
-    private int correctPlace;
-    public GameObject[] parkingNumbers;
+    private int[] incorrectNumbers;
+
+
+    
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {   
+        numberFreeParkings = scriptCarInstances.pNumbers.Count;
+        parkingNumbers = scriptCarInstances.pNumbers;
+
+        GenerateOperation();
+        ChangeNumberParking();
+
+    }
+
+    public void GenerateOperation(){
         // Generamos un numero aleatorio para decidir si sera una suma o una resta
         int i = Random.Range(0,2);
 
-        if (i == 0){  // Generamos una suma cuyo resultado tenga como maximo 3 cifras
+        if (i == 0){        // Generamos una suma cuyo resultado tenga como maximo 3 cifras
             firstNumber = Random.Range(110, 800);
             int limit = 999 - firstNumber;
             secondNumber = Random.Range(150,limit);
@@ -27,38 +49,66 @@ public class SetOperation : MonoBehaviour
             sol = firstNumber + secondNumber;
 
         }
-        else{         // Generamos una resta cuyo resultado tenga como maximo 3 cifras
+        else{               // Generamos una resta cuyo resultado tenga como maximo 3 cifras
             firstNumber = Random.Range(501, 999);
             secondNumber = Random.Range(0, firstNumber-50);
 
             sol = firstNumber - secondNumber;
-
         }
-        Debug.Log("La operacion es: " + i + "La operacion es " + firstNumber + " y " + secondNumber);
 
+        Debug.Log("La operacion es: " + i + ". La operacion es " + firstNumber + " y " + secondNumber);        
+        Debug.Log("El resultado es: " + sol);        
+    }
+
+    public void ChooseWrongAnswer(TextMeshPro ptext, int indexWrongN){
+        int incorrectSol = Random.Range(sol - 12, sol +13);
+
+        bool duplicated = incorrectNumbers.Contains(incorrectSol);
+
+        if (duplicated || incorrectSol == sol)      // MUY POCO EFICIENTE, REVISAR!!!!!!!!!!!!!!!!!!!
+            ChooseWrongAnswer(ptext, indexWrongN);
+        else{
+            ptext.text = incorrectSol.ToString();
+            incorrectNumbers[indexWrongN] = incorrectSol;
+        }
+
+    }
+
+
+    public void ChangeNumberParking(){ // Cambia el Tag del parking y escribe el numero de la solucion
 
         // Generamos un numero aleatorio para elegir en que aparcamiento se coloca la solucion correcta
-        int totalParkings = parkingNumber.transform.childCount;
-        correctPlace = Random.Range(0, totalParkings);
+        int correctPlace = Random.Range(0, numberFreeParkings);
+        Debug.Log("El sitio correcto es: " + correctPlace);
 
         // Asignamos el tag ParkedCorrectly al aparcamiento con la solucion correcta y ParkedIncorrectly al resto
-        parkingNumbers = new GameObject[totalParkings];
-        for (int j = 0 ; j < totalParkings ; j++ ){
-            parkingNumbers[j] = parkingNumber.transform.GetChild(j).gameObject;
+        //parkingNumbers = new GameObject[numberFreeParkings];
+        incorrectNumbers = new int[numberFreeParkings -1];
+        int indexWrongN = 0;
+        for (int i = 0 ; i < numberFreeParkings ; i++ ){
+            //parkingNumbers[i] = parkingNumber.transform.GetChild(i).gameObject;
+
+            // Accedemos al numero de la plaza de aparcamiento para luego cambiarlo
+            TextMeshPro ptext = parkingNumbers[i].GetComponentInChildren<TextMeshPro>();
             
-            if( j == correctPlace)
-                parkingNumbers[j].tag = "ParkedCorrectly";
-            else
-                parkingNumbers[j].tag = "ParkedIncorrectly"; 
-        }
-
-
-        //gameObject.tag = "loquesea"
+            // Actualizamos el tag del aparcamiento y el numero de la plaza
+            if( i == correctPlace){
+                parkingNumbers[i].tag = "ParkedCorrectly";
+                ptext.text = sol.ToString();                // Numero con la solucion
+            }
+            else{
+                parkingNumbers[i].tag = "ParkedIncorrectly";
+                ChooseWrongAnswer(ptext, indexWrongN);      // Numeros sin la solucion
+                indexWrongN ++; 
+            }
+        }        
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
