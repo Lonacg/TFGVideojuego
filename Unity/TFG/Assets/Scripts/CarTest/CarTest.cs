c67828aac4de547f03eb17d44217296be78f877e
Lonacg <laurapcg36@gmail.com>
2025-02-03 22:07:46 +0100

    MGParking: continuando cambio del movimiento

--- Unity/TFG/Assets/Scripts/CarTest/CarTest.cs
+++ Unity/TFG/Assets/Scripts/CarTest/CarTest.cs
@@ -1,30 +1,42 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class CarTest : MonoBehaviour
 {
 
-    private float movLat;
-    private float movDelante;
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque;     // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle;   // maximum steer angle the wheel can have
 
-    public float movSpeed = 10;
-    public float rotationSpeed = 250;
     
-    public Rigidbody rb;
 
 
-
-
-    void Update()
-    {
-        movLat = Input.GetAxisRaw("Horizontal");
-        movDelante = Input.GetAxisRaw("Vertical");
-
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(transform.forward * movDelante * movSpeed, ForceMode.Acceleration);
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+            
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
 
     }
+}
+
+
+
+[System.Serializable]
+public class AxleInfo {
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;      // is this wheel attached to motor?
+    public bool steering;   // does this wheel apply steer angle?
 }
--- Unity/TFG/Assets/Scripts/MGParking/CarMovement.cs
+++ Unity/TFG/Assets/Scripts/MGParking/CarMovement.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-
     [Header("Movement:")]
     public float movSpeed = 10;
     public float rotationSpeed = 250;
     public float inertia;
-    private Vector2 movement = Vector2.zero;
-    private Vector3 targetVelocity;
+    private Vector2 inputMovement = Vector2.zero;
     private Rigidbody rb;
 
 
@@ -24,29 +22,27 @@ public class CarMovement : MonoBehaviour
 
     void Update(){
         // Input del teclado para el movimiento
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        inputMovement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
     }
 
     void FixedUpdate()
     {
         // Movemos el coche aplicando la fisica y en el FixedUpdate para que sean siempre los mismos frames
-        Vector3 velocity = movSpeed * (movement.y * transform. forward); 
-        targetVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
-
+        Vector3 velocity = movSpeed * (inputMovement.y * transform. forward); 
         rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
 
-        if(movement.y != 0){ // QUITANDO ESTE IF PIERDE REALISMO PERO GANA MANEJABILIDAD
-            transform.Rotate(0, movement.x * Time.deltaTime * rotationSpeed, 0);
+        if(inputMovement.y != 0){ // QUITANDO ESTE IF PIERDE REALISMO PERO GANA MANEJABILIDAD
+            transform.Rotate(0, inputMovement.x * Time.deltaTime * rotationSpeed, 0);
         }
 
 
         // Giro de las ruedas delanteras
         Quaternion wheelTargetRotation = Quaternion.identity;
-        if (movement.x < 0) // Giro a la izquierda
+        if (inputMovement.x < 0) // Giro a la izquierda
             wheelTargetRotation = Quaternion.Euler(0, -30, 0);
         
-        if (movement.x > 0) // Giro a la derecha
+        if (inputMovement.x > 0) // Giro a la derecha
             wheelTargetRotation = Quaternion.Euler(0, 30, 0);
         
         foreach (var wheel in frontWheels)
