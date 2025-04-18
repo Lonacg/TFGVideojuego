458bf6422ffb42294610838c0ec31238c44a144d
Lonacg <laurapcg36@gmail.com>
2025-04-18 21:28:54 +0200

    MGPuzzle: edicion de las 3 imagenes segun la dificultad y puestas en escena, planteamiento del juego pendiente de terminar, comprobar la posicion no seria buena idea

---
+++ Unity/TFG/Assets/Scripts/Puzzle/PieceCheck.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PieceCheck : MonoBehaviour
+{
+    private Vector2 correctPosition;
+
+
+
+
+
+
+    void Awake()
+    {
+        
+    }
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+
+}
